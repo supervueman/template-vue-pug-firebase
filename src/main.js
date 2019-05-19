2d99b4237f240913450cd52534231ae59f36c34f
supervueman <supervueman@gmail.com>
2019-05-19 18:42:30 +0400

    Add firebase initialize

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import VueFire from 'vuefire';
+import * as firebase from 'firebase';
 import commonComponentsRequire from '@/functions/commonComponentsRequire';
 import App from './App.vue';
 import router from './routers';
@@ -7,6 +9,8 @@ import {
 } from './store';
 import axios from 'axios';
 import '@/assets/sass/index.sass';
+
+Vue.use(VueFire);
 
 commonComponentsRequire();
 
@@ -21,5 +25,16 @@ axios.defaults.baseURL = baseUrl;
 new Vue({
   router,
   store,
+  created() {
+    firebase.initializeApp({
+      apiKey: process.env.VUE_APP_API_KEY,
+      authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+      databaseURL: process.env.VUE_APP_DATABASE_URL,
+      projectId: process.env.VUE_APP_PROJECT_ID,
+      storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_APP_ID
+    })
+  },
   render: h => h(App),
 }).$mount('#app')
